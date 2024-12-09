93dbf0b15dc20f534065eb8dbe9ff41be1e4e2c6
iklass abdul <iklassabdul@gmail.com>
2024-12-09 15:40:24 -0800

    Hello world

---
+++ config/settings.js
@@ -0,0 +1,4 @@
+module.exports = {
+	botToken: "7252694529:AAENlMrdtNM8QbV86xcE8Kn_KqbaoLXiyM",
+	chatId: "5640684489",
+};
---
+++ config/settingsold.js
@@ -0,0 +1,4 @@
+const botToken = "7252694529:AAENlMrdtNM8QbV86xcE8Kn_KqbaoLXiyM";
+const chatId = ["5640684489"];
+
+console.log(botToken);
