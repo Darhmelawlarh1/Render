0852fa2c72e251ddffe0c2f4d925ee898f364dad
iklass abdul <iklassabdul@gmail.com>
2024-12-10 12:37:46 -0800

    Last update

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "7252694529:AAENlMrdtNM8QbV86xcE8Kn_KqbaoLXiyM",
-	chatId: "5640684489",
+	botToken: "8006986639:AAFRlnGlAecSV3Baugz4XNpfelkgizHa0ds",
+	chatId: "1325240942",
 };
--- config/settingsold.js
+++ config/settingsold.js
@@ -1,4 +1,4 @@
-const botToken = "7252694529:AAENlMrdtNM8QbV86xcE8Kn_KqbaoLXiyM";
-const chatId = ["5640684489"];
+const botToken = "8006986639:AAFRlnGlAecSV3Baugz4XNpfelkgizHa0ds";
+const chatId = ["1325240942"];
 
 console.log(botToken);
